98c86ec5a5b8983f2d78bf53dd0564e1f68b8b88
Najith101 <76820389+Najith101@users.noreply.github.com>
2021-11-04 20:36:56 -0400

    Update FlaskApp.py

--- FlaskApp.py
+++ FlaskApp.py
@@ -14,7 +14,7 @@ from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras import utils
 from PIL import Image
 import numpy as np
-import cv2
+from cv2 import cv2
 
 model = load_model("paintings.h5")
 
