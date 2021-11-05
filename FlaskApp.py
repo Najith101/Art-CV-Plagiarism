2523ffb7d5e6eae63c274222d193fccbb5d0dbe6
Najith101 <76820389+Najith101@users.noreply.github.com>
2021-11-04 20:11:44 -0400

    updating the predict part of FlaskApp.py

--- FlaskApp.py
+++ FlaskApp.py
@@ -16,23 +16,31 @@ from PIL import Image
 import numpy as np
 
 model = load_model("paintings.h5")
-model.make_predict_function()
-categories = ["VanGohg" , "Not"]
 
 def preprocess(img):
-  img = np.asarray(img)
+  #grayscaling images
+  grayimg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  #initiating ORB detector
+  orb = cv2.ORB_create(nfeatures=1000)
+  #finding keypoints and detectors for both images
+  kpt, desc = orb.detectAndCompute(grayimg,None)
+  kp_img=[]
+  img = cv2.drawKeypoints(grayimg, kpt, None)
+  kp_img.append(np.asarray(img))
+  img = np.asarray(kp_img)
   img = utils.normalize(img, axis=1)
   return img
 
-def predict(location):
+def predict(location, model):
   img = load_img(location,target_size=(400, 400))
   img = preprocess(img)
   pred = model.predict(img)
   predictedClass = np.argmax(pred[0])
-  if predicted_class == 0:
+  categories = ["VanGohg" , "Not"]
+  if predictedClass == 0:
     pred_made = categories[0]
   else:
-    pre_made = categories[1]
+    pred_made = categories[1]
   return pred_made
 
 app = Flask(__name__)
